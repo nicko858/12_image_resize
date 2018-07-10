cd2f3d9cbdc7e6d056c5447072b7c351bc57368d
nicko858 <ikar2021@hotmail.com>
2018-07-10 16:20:07 +0300

    script&requirements&readme

--- README.md
+++ README.md
@@ -1,6 +1,66 @@
 # Image Resizer
 
-[TODO. There will be project description]
+The program is represented by the module ```image_resize.py```.
+Module ```image_resize.py``` contains the following functions:
+
+- ```check_image()```
+- ```check_optional_args()```
+- ```check_output_path()```
+- ```check_positive()```
+- ```get_args()```
+- ```get_img_file_size()```
+- ```get_max_size()```
+- ```get_output_file_path()```
+- ```resize_image()```
+
+**The program uses these standart and third-party libraries:**
+
+```python
+PIL
+argparse
+os
+```
+**How in works:**<br/>
+The program accepts an image and puts it with a new size to a specified directory.If user doesn't specify output directory, the program puts new image to same as source image directory.<br/><br/>
+If only the width is specified, the height is considered to preserve the aspect ratio of the image. And vice versa. - If both width and height are specified - create this image. Display a warning in the console if the proportions do not match the original image.<br/><br/>
+    If the scale is specified, the width and height can not be specified. Otherwise, no resize occurs and the script breaks with an understandable error.
+    If no path is specified before the final file, the result is placed to same as source image directory. If the source file is called pic.jpg (100x200), then after the call to python image_resize.py -scale 2 pic.jpg the file pic__200x400.jpg should appear.<br/><br/>
+**It has a mandatory argument:**<br/>
+```source_image ```- the path to the original picture. <br/><br/>
+**And a few optional:** 
+- ```width ```- width of the resulting image 
+- ```height ```- its height
+- ```scale``` - how many times to increase the image (maybe less than 1) 
+- ```output``` - where to put the resulting file. 
+
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+# How to run
+- Activate virtualenv
+``` bash
+source <path_to_virtualenv>/bin/activate
+```
+- Run script with virtualenv-interpreter
+```bash
+<path_to_virtualenv>/bin/python3.5 image_resize.py <path_to_image> -height 170 
+```
+If everything is fine, you'll see such output:<br/><br/>
+```Ok! The new file - '/home/nicko/devman//profit__170x170.png'!```
+
+In case of wrong directory or invalid image file, you'll see error messages:<br/>
+
+```You don't have permission to save into the '/' directory```<br/>
+or <br/>
+```The source image file is invalid!```
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,167 @@
+from PIL import Image
+import argparse
+from argparse import ArgumentTypeError
+from os.path import (exists, isfile,
+                     basename, splitext, dirname, isdir)
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def check_image(source_file):
+    if not exists(source_file):
+        msg_exist = ("No such file or directory"
+                     " - '{}' !".format(source_file))
+        raise ArgumentTypeError(msg_exist)
+    elif not isfile(source_file):
+        msg_isdir = "'{}' is not a file".format(source_file)
+        raise ArgumentTypeError(msg_isdir)
+    else:
+        return source_file
 
 
-if __name__ == '__main__':
-    pass
+def check_positive(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise ArgumentTypeError("{} is an invalid value! "
+                                "It must be positive!".format(value))
+    return ivalue
+
+
+def check_optional_args(width=None, height=None, scale=None):
+    msg_error = ("You can not specify scale with height or width "
+                 "the same time! ")
+    if scale:
+        if width or height:
+            return msg_error
+    return "Ok"
+
+
+def check_output_path(output):
+    output_dir = dirname(output)
+    if not exists(output_dir):
+        msg_exist = ("No such file or directory - "
+                     "'{}' !".format(output_dir))
+        raise ArgumentTypeError(msg_exist)
+    elif not isdir(output_dir):
+        msg_isdir = "'{}' is not a directory".format(output_dir)
+        raise ArgumentTypeError(msg_isdir)
+    else:
+        return output
+
+
+def get_args():
+    script_usage = """python image_resize.py 
+                       Mandatory parameter:
+                       -source_image <path to source image>
+    
+                       Optional parameters:
+                       -width <value>
+                       -height <value>
+                       -scale <value>
+                       -output <path to resize file>"""
+    parser = argparse.ArgumentParser(
+        description="How to run image_resize.py:",
+        usage=script_usage
+    )
+    parser.add_argument(
+        "source_image",
+        type=check_image
+    )
+
+    parser.add_argument(
+        "-width",
+        type=check_positive
+    )
+    parser.add_argument(
+        "-height",
+        type=check_positive
+    )
+    parser.add_argument(
+        "-scale",
+        type=check_positive
+    )
+    parser.add_argument(
+        "-output",
+        type=check_output_path
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def get_img_file_size(source_image):
+    with Image.open(source_image) as im:
+        width, height = im.size
+    return width, height
+
+
+def get_output_file_path(source_image, size, output=None):
+    width, height = size
+    source_file_name = basename(source_image)
+    file_name, file_extension = splitext(source_file_name)
+    output_file_name = "{}__{}x{}{}".format(file_name,
+                                            width,
+                                            height,
+                                            file_extension)
+    if output:
+        return "{}/{}".format(output, output_file_name)
+    return "{}/{}".format(dirname(source_image), output_file_name)
+
+
+def get_max_size(path_to_original,
+                 width=None,
+                 height=None):
+    original_width, original_height = get_img_file_size(path_to_original)
+    max_size = ()
+    if width and height:
+        max_size = (width, height)
+    elif width:
+        max_size = (width, original_height)
+    elif height:
+        max_size = (original_width, height)
+    elif scale:
+        max_size = tuple([scale * i for i in
+                          (original_width, original_height)])
+    return max_size
+
+
+def resize_image(path_to_original,
+                 output_image_path=None,
+                 width=None,
+                 height=None,
+                 scale=None):
+    max_size = get_max_size(path_to_original, width, height)
+    with Image.open(path_to_original) as original_image:
+        if scale or (width and height):
+            resized_image = original_image.resize(max_size)
+            image = resized_image
+        else:
+            original_image.thumbnail(max_size, Image.ANTIALIAS)
+            image = original_image
+        output = get_output_file_path(path_to_original,
+                                      image.size,
+                                      output_image_path)
+        image.save(output)
+    return output
+
+
+if __name__ == "__main__":
+    args = get_args()
+    source_image = args.source_image
+    width = args.width
+    height = args.height
+    scale = args.scale
+    output = args.output
+    validate_args = check_optional_args(width, height, scale)
+    if validate_args is not "Ok":
+        exit(validate_args)
+    try:
+        result_image = resize_image(source_image,
+                                    output,
+                                    width,
+                                    height,
+                                    scale)
+        print("Ok! The new file - '{}'!".format(result_image))
+    except PermissionError:
+        exit("You don't have permission to save into the"
+             " '{}' directory".format(output))
+    except IOError:
+        exit("The source image file is invalid!")
